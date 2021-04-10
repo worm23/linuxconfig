6f045ae1cb1e7b313c96264949c47614a1d75526
worm23 <p@w23.at>
2021-04-10 15:07:21 +0200

    Update installvimrc.sh

--- installvimrc.sh
+++ installvimrc.sh
@@ -3,6 +3,8 @@ wget https://raw.githubusercontent.com/worm23/linuxconfig/main/vimrc.local
 chown root:root vimrc.local
 chmod 644 vimrc.local
 mv -f vimrc.local /etc/vim/vimrc.local
+# there needs to be a .vimrc file, othervise other defaults are used that override vimrc.local
+touch ~/.vimrc
 
 wget https://raw.github.com/altercation/vim-colors-solarized/master/colors/solarized.vim
 chown root:root solarized.vim
