3380d9fa4f58e1056b5fa325f1caca46a24f2e4d
worm23 <p@w23.at>
2021-04-10 15:03:37 +0200

    Update installvimrc.sh

--- installvimrc.sh
+++ installvimrc.sh
@@ -2,11 +2,11 @@
 wget https://raw.githubusercontent.com/worm23/linuxconfig/main/vimrc.local
 chown root:root vimrc.local
 chmod 644 vimrc.local
-mv vimrc.local /etc/vim/vimrc.local
+mv -f vimrc.local /etc/vim/vimrc.local
 
 wget https://raw.github.com/altercation/vim-colors-solarized/master/colors/solarized.vim
 chown root:root solarized.vim
 chmod 644 solarized.vim
 VIMRUNTIME=`vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015' `
-mv solarized.vim $VIMRUNTIME/colors/
+mv -f solarized.vim $VIMRUNTIME/colors/
 
