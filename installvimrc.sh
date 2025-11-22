b167998be07794ac02eac99a6c627de11cda1d19
Worm23 <p@w23.at>
2025-11-22 12:47:48 +0100

    Update installvimrc.sh

--- installvimrc.sh
+++ installvimrc.sh
@@ -7,6 +7,7 @@ if [ "$EUID" -ne 0 ]
 then
 	echo "Please run also as root!"
 else
+	# FYI github caches files for 5 minutes, it's not possible to work around that cache
 	wget -nv https://raw.githubusercontent.com/worm23/linuxconfig/main/vimrc.local
 	chown root:root vimrc.local
 	chmod 644 vimrc.local
@@ -16,7 +17,7 @@ else
 	chown root:root solarized.vim
 	chmod 644 solarized.vim
 	VIMCOLORS=`vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015' `
-	VIMCOLORS=$(echo $VIMCOLORS | xargs)/colors/
+	VIMCOLORS=$(echo $VIMCOLORS | xargs)/colors/ # trim path and add colors
 
 	if [ -d $VIMCOLORS ]; then
   		mv -f solarized.vim $VIMCOLORS
