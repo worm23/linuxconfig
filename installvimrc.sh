92f2e2b6f9807194e596a440f47d2349e536a9ae
Worm23 <p@w23.at>
2025-11-22 12:31:14 +0100

    Update installvimrc.sh - check vim colors path

--- installvimrc.sh
+++ installvimrc.sh
@@ -7,6 +7,7 @@ if [ "$EUID" -ne 0 ]
 then
 	echo "Please run also as root!"
 else
+	source ~/.bashrc
 	wget https://raw.githubusercontent.com/worm23/linuxconfig/main/vimrc.local
 	chown root:root vimrc.local
 	chmod 644 vimrc.local
@@ -16,8 +17,13 @@ else
 	chown root:root solarized.vim
 	chmod 644 solarized.vim
 	VIMRUNTIME=`vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015' `
-	mkdir --parents $VIMRUNTIME/colors/
-	mv -f solarized.vim $VIMRUNTIME/colors/
+	VIMRUNTIME=$(echo $VIMRUNTIME | xargs)/colors/
+
+	if [ -d $VIMRUNTIME/colors/ ]; then
+  		mv -f solarized.vim $VIMRUNTIME/colors/
+	else
+		echo "VIM color path $VIMRUNTIME/colors/ does not exist"
+	fi
 	echo "Please also run as user!"
 fi
 
