629d9d5adbd804d8dd37fd7cc87b2bf60f5866fa
Worm23 <p@w23.at>
2025-11-22 12:42:41 +0100

    Update installvimrc.sh - reduce output

--- installvimrc.sh
+++ installvimrc.sh
@@ -7,12 +7,12 @@ if [ "$EUID" -ne 0 ]
 then
 	echo "Please run also as root!"
 else
-	wget https://raw.githubusercontent.com/worm23/linuxconfig/main/vimrc.local
+	wget -nv https://raw.githubusercontent.com/worm23/linuxconfig/main/vimrc.local
 	chown root:root vimrc.local
 	chmod 644 vimrc.local
 	mv -f vimrc.local /etc/vim/vimrc.local
 
-	wget https://raw.github.com/altercation/vim-colors-solarized/master/colors/solarized.vim
+	wget -nv https://raw.github.com/altercation/vim-colors-solarized/master/colors/solarized.vim
 	chown root:root solarized.vim
 	chmod 644 solarized.vim
 	VIMCOLORS=`vim -e -T dumb --cmd 'exe "set t_cm=\<C-M>"|echo $VIMRUNTIME|quit' | tr -d '\015' `
